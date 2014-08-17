c7d8809a0b8029732484f242a0956aa24c4815f0
Rafael Osuna <rafa.osuna@gmail.com>
2014-08-17 04:53:53 -0500

    HelloWorld repository
    HelloWorld repository

---
+++ HelloWorld/src/main/java/com/rafaelosuna/HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.rafaelosuna.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
